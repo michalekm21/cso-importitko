3e312b8590f36a2b52bf5748f1332257ad4b9344
Jan Michálek <jan.michalek@tmapy.cz>
2024-05-01 23:20:12 +0200

    ukazkova aplikace

---
+++ lsdImport/tools.py
@@ -0,0 +1,19 @@
+import logging
+
+
+class LoggerConfig:
+    @staticmethod
+    def get_logger(name, handler=None):
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)  # Nastavení úrovně logování
+        if handler:
+            logger.addHandler(handler)  # Přidání specifikovaného handleru
+        else:
+            # Vytvoření handleru pro logování do konzole
+            console_handler = logging.StreamHandler()
+            formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)  #
+
+        return logger
