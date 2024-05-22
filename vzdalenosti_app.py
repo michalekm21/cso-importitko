835fb441786ea18cf99788dd6f9a6291265ceaf5
Michálek Martin <michalekm21@spstrutnov.cz>
2024-05-22 20:28:19 +0200

    krovak

--- vzdalenosti_app.py
+++ vzdalenosti_app.py
@@ -28,8 +28,9 @@ class GeometryDistanceCalculator:
 
         self.target_srs = osr.SpatialReference()
         self.target_srs.ImportFromProj4(
-            "+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 "
-            "+k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs"
+            "+proj=krovak +lat_0=49.5 +lon_0=24.8333333333333 "
+            "+alpha=30.2881397527778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel "
+            "+towgs84=589,76,480,0,0,0,0 +units=m +no_defs +type=crs"
             )
 
         self.transform = osr.CoordinateTransformation(
