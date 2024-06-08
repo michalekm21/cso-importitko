1b437d97c35b7fa127fcc05912765c8c9e82ffdd
Michálek Martin <michalekm21@spstrutnov.cz>
2024-06-08 11:22:51 +0200

    knihovna halo

--- utils/distance_calculator.py
+++ utils/distance_calculator.py
@@ -4,7 +4,7 @@ import os
 import warnings
 import logging
 from osgeo import osr, ogr
-from lib.halo import Halo
+from utils.lib.halo import Halo
 
 os.environ['SHAPE_ENCODING'] = "utf-8"
 osr.UseExceptions()
